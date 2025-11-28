c4047ed68fdacb9cdf862f133858f495ae300e6a
Ddiy0ng <bueong0627@gmail.com>
2025-11-28 12:49:26 +0900

    수정

--- app/src/main/java/com/example/habitcheck/MainActivity.kt
+++ app/src/main/java/com/example/habitcheck/MainActivity.kt
@@ -12,8 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.habitcheck.ui.theme.MyApplicationTheme
 import com.example.habitcheck.R
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
